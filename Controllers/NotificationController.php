71f97a5d12bb39b85b59ef6af3cae18fcdd2bc64
ezra <ezra@ezra-mach>
2014-01-22 16:14:20 +0100

    Updates

---
+++ Controllers/NotificationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DSLive\Controllers;
+
+use DScribe\Core\AController;
+
+class NotificationController extends AController {
+
+    public function noCache() {
+        return array('register');
+    }
+
+    public function registerAction() {
+        
+    }
+
+}
---
+++ Forms/FileForm.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ */
+
+namespace DSLive\Forms;
+
+use DScribe\Form\Form,
+	DSLive\Models\File;
+
+/**
+ * Description of FileForm
+ *
+ * @author topman
+ */
+class FileForm extends Form {
+
+	public function __construct(File $model, $name = 'fileForm', array $attributes = array()) {
+		parent::__construct($name, $attributes);
+
+		$this->setModel($model);
+
+		$this->setAttributes(array(
+			'method' => 'post',
+			'enctype' => 'multipart/form-data'
+		));
+
+		$this->add(array(
+			'name' => 'MAX_FILE_SIZE',
+			'type' => 'hidden',
+			'attributes' => array(
+				'value' => $model->getMaxSize(),
+			)
+		));
+	}
+
+}
